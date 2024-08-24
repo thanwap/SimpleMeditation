8a8836d618f64a42b5d1f034a50793a5d8c70390
thanwa.p <dhunwa_p@hotmail.com>
2024-08-24 16:47:06 +0700

    chore: add prettier

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import { View, Text } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { ActivityIndicator, MD2Colors } from "react-native-paper";
+import { useState } from 'react'
+import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { ActivityIndicator, MD2Colors } from 'react-native-paper'
 
 export default function HomeScreen() {
-  const [targetSeconds, setTargetSeconds] = useState(60);
-  const [currentSeconds, setCurrentSeconds] = useState(0);
+  const [targetSeconds, setTargetSeconds] = useState(60)
+  const [currentSeconds, setCurrentSeconds] = useState(0)
 
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className='flex-1 items-center justify-center bg-white'>
       <Text>Let's Meditate!</Text>
       <Text>Target: {targetSeconds} seconds</Text>
       <Text>{currentSeconds} seconds</Text>
       <ActivityIndicator animating={true} color={MD2Colors.red800} />
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
